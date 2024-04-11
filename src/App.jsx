ac623cc7fe1d508feef8cf2bfdab66fb531cde3a
rahulporel51 <rahulporel51@gmail.com>
2024-04-11 13:18:07 +0530

    app  update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import Quiz from "./components/Quiz";
 import { qnaList } from "./utils/qnaList";
+import WelcomeInput from "./components/WelcomeInput";
 
 function App() {
-  return <Quiz AllQuestions={qnaList.allQuestions} />;
+  const [isUser, setIsUser] = useState(null);
+  return (
+    <>
+      {" "}
+      <WelcomeInput setIsUser={setIsUser} isUser={isUser} />
+      <Quiz
+        AllQuestions={qnaList.allQuestions}
+        setIsUser={setIsUser}
+        isUser={isUser}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/WelcomeInput.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WelcomeInput = ({ isUser, setIsUser }) => {
+  return (
+    <div>
+      <h1>Welcome to Interview Prepartion</h1>
+      <h3>We will help you to execel on your technical interview</h3>
+
+      <h2>You can customize your test on your perfrence </h2>
+      
+    </div>
+  );
+};
+
+export default WelcomeInput;
